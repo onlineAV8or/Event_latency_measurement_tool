77a5f7683a3860ecfa103324940be61b6bc615bb
onlineAV8or <linkedin.ak.100@gmail.com>
2025-04-01 23:42:00 +0530

    createTest, loadTest classes added

--- rttTool.py
+++ rttTool.py
@@ -93,4 +93,97 @@ dir_path_cam = r".\Apprelated"
 
 #createTestRecordClass
 class createTestRecordClass(FloatLayout):
-    
+    recordedVideo: TextInput
+    testName: TextInput
+    date: TextInput
+    vidRelatedInfo: TextInput
+
+    def file(self):
+        root = tk.Tk()
+        root.withdraw()
+        default_ext = ".mp4"
+        self.recordedVideo.text=""
+        selected_dir = filedialog.askopenfilename(filetypes=[("MP4 files","*.mp4")], defaultextension=default_ext,initialdir=dir_path_cam, title="Select Directory")
+        self.recordedVideo.text = selected_dir
+        print(selected_dir)
+
+    def saveTestData(self):
+        if(self.recordedVideo.text != ""):
+            dictTest = {'TestName': self.testName.text, 'Date Created': self.date.text, 'Video Path': self.recordedVideo.text, 'Video Related Info': self.vidRelatedInfo.text}
+            testName = dict['TestName']
+            with open(f'{dir_path_cam}/{testName}.json', "w") as outfile:
+                json.dump(dictTest,outfile)
+
+            self.testName.text = ""
+            self.date.text = ""
+            self.recordedVideo.text = ""
+            self.vidRelatedInfo.text = ""
+            print("Test Saved")
+            MyGridLayout.popupWindowCreateTestRec.dismiss()
+        else:
+            print("No file chosen")
+            MyGridLayout.popupWindowCreateTestRec.dismiss()
+
+    def cancelApp(self,windowName):
+        self.testName.text = ""
+        self.date.text =""
+        self.recordedVideo.text = ""
+        self.vidRelatedInfo.text =""
+        MyGridLayout.popupWindowCreateTestRec.dismiss()
+
+
+class loadTestRecordClass(FloatLayout):
+    textTest: TextInput
+
+    def file(self):
+        root = tk.Tk()
+        root.withdraw()
+        default_ext = ".json"
+        selected_dir = filedialog.askopenfilename(filetypes=[("Json files", "*.json")], defaultextension=default_ext,
+                                                  initialdir=dir_path_cam, title="Select Directory")
+        self.textTest.text = selected_dir
+        print(selected_dir)
+
+    def saveTestDate(self):
+        if(self.textTest.text!="")
+            with open(f'{self.textTest.text}',"r") as openFile:
+                data = json.load(openFile)
+            videoPath = data['Video Path']
+            testName = data["TestName"]
+            jdata = {"videoPath": videoPath, "testName": testName}
+            with open(jsonFilePath,"w") as writeFile:
+                json.dump(jdata,writeFile)
+            print("written")
+            coordDict = {
+                'x1': int(0),
+                'y1': int(0),
+                'x2': int(0),
+                'y2': int(0)
+            }
+
+            with open(jsonFilePathCoord,"w") as outfile:
+                json.dump(coordDict,outfile)
+
+            coordDict2 = {
+                'x1': 0,
+                'y1': 0,
+                'x2': 0,
+                'y2': 0
+            }
+
+            with open(jsonFilePathCoordController, "w") as outfile2:
+                json.dump(coordDict,outfile2)
+            self.textTest.text = ""
+            MyGridLayout.popupWindowLoadTestRec.dismiss()
+
+        else:
+            print("No file chosen")
+            MyGridLayout.popupWindowLoadTestRec.dismiss()
+
+    def cancel(self):
+        self.textTest.text=""
+        MyGridLayout.popupWindowLoadTestRec.dismiss()
+
+
+class deleteTestConfimation(FloatLayout):
+    pass
